e54bee9012129ef07ebefcb11bbb85b54f720b08
Michele Mogavero s308835 <s308835@studenti.polito.it>
2024-01-11 19:46:24 +0100

    Update app

--- td2/app.py
+++ td2/app.py
@@ -1,27 +1,52 @@
 import streamlit as st
-from nltk.sentiment import SentimentIntensityAnalyzer
 import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import sentiwordnet as swn
+from nltk.corpus import wordnet as wn
 
-# Download VADER lexicon for sentiment analysis
-nltk.download('vader_lexicon')
+# Definire una funzione che mappa i tag del part-of-speech di treebank ai tag di WordNet
+def treebank_to_wn(tag):
+    if tag.startswith('J'):
+        return wn.ADJ
+    elif tag.startswith('V'):
+        return wn.VERB
+    elif tag.startswith('N'):
+        return wn.NOUN
+    elif tag.startswith('R'):
+        return wn.ADV
+    else: 
+        return None
 
-# Initialize the Sentiment Intensity Analyzer
-sia = SentimentIntensityAnalyzer()
+def eval_sentiment(review):
+    sentiment = 0.0
+    tokens_count = 0
+    lemmatizer = WordNetLemmatizer()
+    token = nltk.word_tokenize(review)
+    after_tagging = nltk.pos_tag(token)
+    for word, tag in after_tagging:
+        wn_tag = treebank_to_wn(tag)
+        if wn_tag not in (wn.ADJ, wn.ADV):
+            continue
+        lemma = lemmatizer.lemmatize(word, pos=wn_tag)
+        if not lemma:
+            continue
+        synsets = wn.synsets(lemma, pos=wn_tag)
+        if not synsets:
+            continue
+        swn_synset = swn.senti_synset(synsets[0].name())
+        sentiment += swn_synset.pos_score() - swn_synset.neg_score()
+        tokens_count += 1
 
-# Title and description of the app
+    return sentiment
+
 st.title("Sentiment Analysis App")
-st.write("This app uses the NLTK library to analyze sentiment of the text you enter.")
+st.write("Enter the text you'd like to analyze for sentiment.")
 
-# User input for text analysis
-user_input = st.text_area("Enter text here:", "Type your text...")
+user_input = st.text_area("Text to analyze", "Type Here...")
 
-# When the 'Analyze' button is clicked, analyze the sentiment of the input text
-if st.button('Analyze Sentiment'):
+if st.button('Evaluate Sentiment'):
     if user_input:
-        # Perform sentiment analysis
-        sentiment_scores = sia.polarity_scores(user_input)
-        # Display results
-        st.subheader("Sentiment Analysis Results:")
-        st.write(sentiment_scores)
+        result = eval_sentiment(user_input)
+        st.write("The sentiment score of the input is:", result)
     else:
-        st.error("Error!")
+        st.error("ERROR")
